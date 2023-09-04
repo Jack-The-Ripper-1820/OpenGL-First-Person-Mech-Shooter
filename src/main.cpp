a33b842dde4e07687489220023a1be4b0a327736
Mayur <mayurjreddy@gmail.com>
2023-09-04 14:25:33 -0400

    extra uniform variables removed

--- src/main.cpp
+++ src/main.cpp
@@ -358,9 +358,6 @@ int main() {
 
 	skyBox = Skybox(skyBoxFaces);
 
-	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
-		uniformSpecularIntensity = 0, uniformShininess = 0;
-
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
 
 	// Loop until window closed
